49ddb74f16e50cc86f772fff315905cf2d09f2ee
Le Sourd <louis.le-sourd@epitech.eu>
2020-10-02 14:29:14 +0200

    update various user attributes

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -30,23 +30,23 @@ router.post('/updateProfil', function(req, res, next) {
 });
 
 router.post('/updateDescrip', function(req, res, next) {
-    bdd.query("INSERT INTO User (user_description) VALUES('" + req.body.name + "', 'test', '8 contours du chatelet', 35310, 'Bréal sous Monfort', 'France', 'DUT info', 'test', 'test.test@epitech.eu', 'Description de moi', 'Utilisateur', DATE '1999-08-18');", (err, result, fields) => {
+    bdd.query("UPDATE User SET user_description ='" + req.body.description + "' WHERE user_id = " + req.body.id + "';", (err, result, fields) => {
         if (err) throw err;
         console.log(result);
         res.send(result);
     })
 });
 
-router.post('/updateresume', function(req, res, next) {
-    bdd.query("INSERT INTO User (user_cv) VALUES('" + req.body.name + "', 'test', '8 contours du chatelet', 35310, 'Bréal sous Monfort', 'France', 'DUT info', 'test', 'test.test@epitech.eu', 'Description de moi', 'Utilisateur', DATE '1999-08-18');", (err, result, fields) => {
+router.post('/updateCV', function(req, res, next) {
+    bdd.query("UPDATE User SET user_cv ='" + req.body.cv + "' WHERE user_id = " + req.body.id + "';", (err, result, fields) => {
         if (err) throw err;
         console.log(result);
         res.send(result);
     })
 });
 
-router.post('/updateEmail&psswd', function(req, res, next) {
-    bdd.query("INSERT INTO User (user_adress, user_posta_code, user_city, user_country, user_graduation, user_birth) VALUES('" + req.body.name + "', 'test', '8 contours du chatelet', 35310, 'Bréal sous Monfort', 'France', 'DUT info', 'test', 'test.test@epitech.eu', 'Description de moi', 'Utilisateur', DATE '1999-08-18');", (err, result, fields) => {
+router.post('/updateEmail&password', function(req, res, next) {
+    bdd.query("UPDATE User SET user_email ='" + req.body.email + "', user_password = '" + req.body.password + "' WHERE user_id = " + req.body.id + "';", (err, result, fields) => {
         if (err) throw err;
         console.log(result);
         res.send(result);
